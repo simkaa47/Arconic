66840e21d8d2de485e945f9b6bbc13acb1a651c7
Serafim Prilezhaev <simkaa7@gmail.com>
2024-09-24 11:29:05 +0300

    Добавлена вкладка параметов перемещения

---
+++ Arconic.View/UserControls/Settings/Drive/DriveSettingsTab.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Arconic.View.UserControls.Settings.Drive;
+
+public partial class DriveSettingsTab : UserControl
+{
+    public DriveSettingsTab()
+    {
+        InitializeComponent();
+    }
+}
