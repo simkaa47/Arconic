67405766ff263d425007f4c039427dfce3f9c6e3
Serafim Prilezhaev <simkaa7@gmail.com>
2024-09-25 15:46:19 +0300

    Добавлены кнопки свертывания, развертываняи и закрытия приложения

--- Arconic.View/UserControls/HighBar/HighBarControl.axaml.cs
+++ Arconic.View/UserControls/HighBar/HighBarControl.axaml.cs
@@ -1,6 +1,10 @@
-﻿using Avalonia;
+﻿using Arconic.Core.Abstractions.Common;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.Logging;
 
 namespace Arconic.View.UserControls.HighBar;
 
@@ -9,5 +13,49 @@ public partial class HighBarControl : UserControl
     public HighBarControl()
     {
         InitializeComponent();
+    }
+
+    private async void CloseWindowButtonOnClick(object? sender, RoutedEventArgs e)
+    {
+        if (Application.Current is App app && Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            var dialog = app.GetService<IQuestionDialog>();
+            if (dialog != null)
+            {
+                if (await dialog.AskAsync("Закрытие приложения", "Закрыть приложение?"))
+                {
+                    var logger = app.GetService<ILogger<HighBarControl>>();
+                    if (logger != null)
+                    {
+                        logger.LogInformation("Нажата кнопка закрытия приложения");
+                        desktop.MainWindow?.Close();
+                    }
+                }
+            }
+        }
+    }
+
+    private void MinimizeWindowButtonOnClick(object? sender, RoutedEventArgs e)
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime { MainWindow: not null } desktop)
+        {
+            desktop.MainWindow.WindowState = WindowState.Minimized;
+        }
+    }
+    
+    private void NormalWindowButtonOnClick(object? sender, RoutedEventArgs e)
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop && desktop.MainWindow is not null)
+        {
+            desktop.MainWindow.WindowState = WindowState.Normal;
+        }
+    }
+    
+    private void MaximizeWindowButtonOnClick(object? sender, RoutedEventArgs e)
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop && desktop.MainWindow is not null)
+        {
+            desktop.MainWindow.WindowState = WindowState.FullScreen;
+        }
     }
 }
