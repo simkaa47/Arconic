b4d555a3b3841e8c6d608c57a7af7ec30e4c567b
Serafim Prilezhaev <simkaa7@gmail.com>
2024-12-10 09:33:26 +0300

    Добавлены миграции AbbScanNumber

---
+++ Arconic.Core/Infrastructure/DataContext/Data/Migrations/20241209091102_AddScanMumber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Arconic.Core.Infrastructure.DataContext.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddScanMumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ScanNumber",
+                table: "Scans",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ScanNumber",
+                table: "Scans");
+        }
+    }
+}
