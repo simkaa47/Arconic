abd384f0eb4a58e775621b68650e8b33888dac36
Serafim Prilezhaev <simkaa7@gmail.com>
2025-02-12 15:20:21 +0300

    Добавлены настройки Iba

---
+++ Arconic.Core/Models/PlcData/HighLevel/HighLevelSettings.cs
@@ -0,0 +1,23 @@
+﻿using Arconic.Core.Models.Parameters;
+using S7.Net;
+
+namespace Arconic.Core.Models.PlcData.HighLevel;
+
+public class HighLevelSettings
+{
+    public List<Parameter<Byte>> IbaIp { get;  } = Enumerable.Range(0,4).Select(i=> new Parameter<Byte>(
+            $"IP адрес сервера IBA, ,байт {i}",
+            0,
+            255,
+            DataType.DataBlock,
+            ParameterBase.SettingsDbNum,
+            1396+i))
+        .ToList();
+
+    public Parameter<ushort> IbaInterface { get; } = new Parameter<ushort>("Тип интерфеса сервера IBA",
+        0,
+        1,
+        DataType.DataBlock,
+        ParameterBase.SettingsDbNum,
+        1394);
+}
--- Arconic.Core/Models/PlcData/Settings.cs
+++ Arconic.Core/Models/PlcData/Settings.cs
@@ -1,6 +1,7 @@
 ﻿using Arconic.Core.Models.PlcData.Cooling;
 using Arconic.Core.Models.PlcData.Detectors;
 using Arconic.Core.Models.PlcData.Drive;
+using Arconic.Core.Models.PlcData.HighLevel;
 using Arconic.Core.Models.PlcData.Measure;
 using Arconic.Core.Models.PlcData.Safety;
 using Arconic.Core.Models.PlcData.SingleMeasures;
@@ -19,4 +20,5 @@ public class Settings
     public SafetySettings SafetySettings { get; } = new SafetySettings();
     public SteelMagazineSettings SteelSettings { get; } = new SteelMagazineSettings();
     public SingleMeasuresList SingleMeasures { get; } = new SingleMeasuresList(1400);
+    public HighLevelSettings HighLevelSettings { get; } = new HighLevelSettings();
 }
